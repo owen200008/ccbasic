ff00a0db16c8f932b3257f7a04e4ea046e07ef32
owen200008 <89424516@qq.com>
2017-11-26 13:48:54 +0800

    cmake

--- buildios/CMakeFiles/3.9.3/CompilerIdC/CompilerIdC.build/Debug/CompilerIdC.build/Objects-normal/x86_64/CMakeCCompilerId.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Volumes/data/github/ccbasic/buildios/CMakeFiles/3.9.3/CompilerIdC/CMakeCCompilerId.c
--- buildios/CMakeFiles/3.9.3/CompilerIdCXX/CompilerIdCXX.build/Debug/CompilerIdCXX.build/Objects-normal/x86_64/CMakeCXXCompilerId.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Volumes/data/github/ccbasic/buildios/CMakeFiles/3.9.3/CompilerIdCXX/CMakeCXXCompilerId.cpp
